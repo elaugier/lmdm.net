1db59a4a04ce218067fef22bcaa39e31259aae9d
Eric Laugier <eric.laugier@egen.guru>
2023-07-23 08:30:14 +0200

    fix default theming

--- libs/shared/ui/src/lib/button/button.tsx
+++ libs/shared/ui/src/lib/button/button.tsx
@@ -3,12 +3,17 @@ import styles from './button.module.scss';
 /* eslint-disable-next-line */
 export interface ButtonProps {
   displayName: string
-  name: string
+  name?: string
+  theme?: string
 }
 
 export function Button(props: ButtonProps) {
+  const theme = (typeof (props.theme) !== 'undefined') ? props.theme : 'default'
+  const classes = 'theme-' + theme;
   return (
-    <button className={styles['button']} id={props.name}>{props.displayName}</button>
+    <div className={styles[classes]}>
+      <button className={styles['button']} id={props.name}>{props.displayName}</button>
+    </div>
   );
 }
 
